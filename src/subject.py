14264e00a58d44777ebba6a3acc55666ba16c064
DenitsaKostova <dkostova93@gmail.com>
2015-06-05 21:50:11 +0300

    updates subject.py

--- src/subject.py
+++ src/subject.py
@@ -1,38 +1,3 @@
-class Student:
-	def __init__(self, first_name, last_name, gender):
-		self.first_name = first_name
-		self.last_name = last_name
-		self.gender = gender
-
-	def __str__(self):
-        return str(self.first_name) + " " + str(self.last_name) + " " + str(self.gender)
-
-	@property
-	def get_first_name(self):
-	    return self.get_first_name	
-
-	@property
-	def get_last_name(self):
-		return self.get_last_name
-
-	@property
-	def get_gender(self):
-	    return self.get_gender
-	
-
-class Parent:
-	def __init__(self, name, email):
-		self.name = name
-		self.email = email
-
-	@property
-	def get_name(self):
-	    return self.get_name
-
-	@property
-	def get_email(self):
-	    return self.get_email
-
 class Subject:
 	def __init__(self, title, grade):
 		self.title = title
@@ -45,16 +10,3 @@ class Subject:
 	@property
 	def get_grade(self):
 	    return self.get_grade
-	
-	
-
-	
-	
-	
-	
-
-
-
-		
-
-
